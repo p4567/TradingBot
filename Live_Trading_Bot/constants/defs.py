415699dcca8ff31fe8376e06e8d92e54520a8908
unknown <makhanpav200@gmail.com>
2023-11-07 23:05:26 +0000

    first commit

---
+++ Live_Trading_Bot/constants/defs.py
@@ -0,0 +1,7 @@
+API_KEY = "bb70b83c1bd6c0c03e0116a9cc9e0c00-3db6f02983f88d0406f7fd8f6f2bdf54"
+ACCOUNT_ID = "101-004-26296762-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SELL = -1
+BUY = 1
+NONE = 0
